4a6c65441832c2e48e3682a1d0344a547ec7b383
rubenSinzig <ruben.sinzig@posteo.de>
2021-04-26 13:51:33 +0200

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,13 @@
-
+// function for ToDo list
+const addToList = () => {
+  let userData = document.querySelector("#userData").value;
+  if (userData != "") {
+    let newLi = document.createElement("li");
+    let text = document.createTextNode(userData);
+    newLi.appendChild(text);
+    document.querySelector(".result").appendChild(newLi);
+    document.querySelector("#userData").value = "";
+  } else {
+    document.querySelector("#userData").placeholder = "Please Enter Item";
+  }
+};
