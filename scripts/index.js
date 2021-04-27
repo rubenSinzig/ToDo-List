a62e89c62efc6255e10a4b2b7161949fc3ecf611
rubenSinzig <ruben.sinzig@posteo.de>
2021-04-27 14:10:57 +0200

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,44 @@
 const addToList = () => {
   let userData = document.querySelector("#userData").value;
   if (userData != "") {
+    //
     let newLi = document.createElement("li");
     let text = document.createTextNode(userData);
     newLi.appendChild(text);
+    // adding 1 of 4 given color to the list item
+    newLi.style.color = colorGen();
+    // add the li with user input to ul
     document.querySelector(".result").appendChild(newLi);
+    // cleaning the input field after user add
     document.querySelector("#userData").value = "";
+    // declaring var & creat new elements
+    const newContainer = document.createElement("div");
+    const doneButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+    // adding text to the buttons
+    doneButton.innerHTML = "✓";
+    deleteButton.innerHTML = "✗";
+    // Injecting the buttons into the container
+    newContainer.appendChild(doneButton);
+    newContainer.appendChild(deleteButton);
+    // Adding class
+    doneButton.classList.add("done");
+    newLi.appendChild(newContainer);
+    // delete and check
+    const check = () => newLi.classList.toggle("completed");
+    const del = () => newLi.remove();
+    //
+    deleteButton.addEventListener("click", del);
+    doneButton.addEventListener("click", check);
   } else {
     document.querySelector("#userData").placeholder = "Please Enter Item";
   }
+};
+
+// random color
+const colorGen = () => {
+  let result = "";
+  let colorCodeArr = ["#fad176", "#f5708b", "#abd383", "#cec2ed"];
+  result += colorCodeArr[Math.floor(Math.random() * colorCodeArr.length)];
+  return result;
 };
