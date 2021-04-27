0f67c6e824efb5a8bbc5e173bd89daa731132d2c
rubenSinzig <ruben.sinzig@posteo.de>
2021-04-27 11:51:05 +0200

    JavaScript -> function for random color

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,9 +5,18 @@ const addToList = () => {
     let newLi = document.createElement("li");
     let text = document.createTextNode(userData);
     newLi.appendChild(text);
+    newLi.style.color = colorGen();
     document.querySelector(".result").appendChild(newLi);
     document.querySelector("#userData").value = "";
   } else {
     document.querySelector("#userData").placeholder = "Please Enter Item";
   }
+};
+
+// random color
+const colorGen = () => {
+  let result = "";
+  let colorCodeArr = ["#fad176", "#f5708b", "#abd383", "#cec2ed"];
+  result += colorCodeArr[Math.floor(Math.random() * colorCodeArr.length)];
+  return result;
 };
